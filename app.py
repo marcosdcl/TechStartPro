02dad9d556c2d5794fbbbdd2a2d7de4f1c88dc54
Marcos Douglas <marcosdouglasclima@gmail.com>
2020-11-01 15:47:27 -0300

    Add csv reader

--- app.py
+++ app.py
@@ -1,17 +1,36 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from models import db, Category, Product
+import csv
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+web_app = Flask(__name__)
+web_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 def init_db():
     """ Initializes my database coming from models """
-    db.init_app(app)
-    db.app = app
+    db.init_app(web_app)
+    db.app = web_app
     db.create_all()
+
+    with open('categorias.csv', 'r') as csv_file:
+        reader = csv.DictReader(csv_file, delimiter=',')
+        for collumn in reader:
+            category = Category(name=collumn['nome'])
+            db.session.add(category)
+            db.session.commit()
+
+
+@web_app.route('/', methods=['GET'])
+def index():
+    if request.method == 'GET':
+        products = Product.query.all()
+        categories = Category.query.all()
+        return render_template('index.html', products=products, categories=categories)
+
+
+
 
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    web_app.run(debug=True)
