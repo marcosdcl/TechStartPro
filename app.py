454375bdc78d602cf4c045a104628570e23055f9
Marcos Douglas <marcosdouglasclima@gmail.com>
2020-11-01 03:10:14 -0300

    Add model class and category table

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Category(db.Model):
+    """ Create the category table """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 if __name__ == '__main__':
     app.run(debug=True)
